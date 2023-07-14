445ef4bfe5fbb55a83a4f0f7989677e0e65591c5
Wawerugeek <stevemuiruri84@gmail.com>
2023-07-14 22:45:47 +0300

    proper format

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,50 +1,49 @@
 #this puppet script installs nginx
 
-package {'nginx:
-ensure => present,
-name => 'nginx',
+package { 'nginx':
+  ensure => present,
 }
 
-service {'nginx':
-ensure => running,
-enable => true,
-hasrestart => true,
-require => package['nginx'],
-subscribe => file_line["add redirect"],
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['add redirect'],
 }
 
-file {'/var/www/html/index.html':
-ensure => present,
-path => '/var/www/html/index.html',
-content => Hello World!
+file { '/var/www/html/index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!',
 }
 
 file { '/etc/nginx/sites-available/default':
-ensure => present,
-content => '
-server {
-listen 80 default_server;
-listen [::]:80 default_server;
+  ensure  => present,
+  content => '
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      server_name _;
 
-server_name _;
+      location / {
+        root /var/www/html;
+        index index.html;
+      }
 
-location / {
-root /var/www/html;
-index index.html;
-}
-
-location /redirect_me {
-return 301 http://muiruri.tech/redirect_me.html;
-}
-}'
-,
-require => package['nginx'],
-notify => service['nginx'],
+      location /redirect_me {
+        return 301 http://muiruri.tech/redirect_me.html;
+      }
+    }
+  ',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
 file { '/etc/nginx/sites-enabled/default':
-ensure => 'link'
-target => '/etc/nginx/sites-available/default',
-require => file['/etc/nginx/sites-available/default'],
-notify => service['nginx']
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx'],
 }
+
