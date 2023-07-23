edd699c09cbe5e4784b2ef34f18c2a927a605e6f
Wawerugeek <stevemuiruri84@gmail.com>
2023-07-23 22:28:54 +0300

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,28 @@
-#this puppet script installs nginx
-package {'nginx':
-ensure => present,
-name   => 'nginx',
+#install nginix with puppet
+#listening on port 80
+#when queried as root it must return Hello world
+#redirection is moved permenately
+
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
 service { 'nginx':
-ensure     => running,
-enable     => true,
-hasrestart => true,
-require    => Package['nginx'],
-subscribe  => File_line["add redirect"],
-}
-
-file {'/var/www/html/index.html':
-ensure  =>  present,
-path    => '/var/www/html/index.html',
-content => 'Holberton School for the win yeah!\n',
-}
-
-file_line {'add redirect':
-ensure  => present,
-path    => '/etc/nginx/sites-available/default',
-replace => true,
-line    => '	rewrite /redirect_me/ http://muiruri.tech permanent;',
-match   => '# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000',
+  ensure  => running,
+  require => Package['nginx'],
 }
