1529378904fb6c61a7de1d8b12eba8fcb6b5b426
wawerugeek <stevemuiruri84@gmail.com>
2023-07-24 16:09:55 +0300

    change format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,17 +10,20 @@ def main():
     emp_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
     tasks_url = (
-        "https://jsonplaceholder.typicode.com/users/{}/todos".format(emp_id))
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(emp_id)
+    )
     res = requests.get(tasks_url)
     tasks = res.json()
     user = requests.get(user_url).json()
-    emp_name = user["name"]
+    EMPLOYEE_NAME = user["name"]
     completed_tasks = [x for x in tasks if x['completed']]
-    tasks_done = len(completed_tasks)
-    total_tasks = len(tasks)
+    NUMBER_OF_DONE_TASKS = len(completed_tasks)
+    TOTAL_NUMBER_OF_TASKS = len(tasks)
 
     print(
-        f"Employee {emp_name} is done with tasks({tasks_done}/{total_tasks}):")
+        f"Employee {EMPLOYEE_NAME} is done \
+        with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):"
+    )
 
     for task in completed_tasks:
         print("\t{}".format(task["title"]))
